66a504adbf11778df0b8c1b16becea66449f2e74
joseamirandavelez <joseamirandavelez@gmail.com>
2026-04-04 22:42:13 -0400

    Rebranding

--- exeWrap.ps1
+++ exeWrap.ps1
@@ -1,11 +1,11 @@
-# $bytes = [System.IO.File]::ReadAllBytes(".\logo.png")
+# $bytes = [System.IO.File]::ReadAllBytes("C:\Users\josea\OneDrive\Documents\EffectInstaller\SignalRGB-Effect-Installer\rgbjunkielogo.png")
 # $base64 = [System.Convert]::ToBase64String($bytes)
 # $base64 | Out-File "encoded_logo.txt"
 
-Invoke-ps2exe -inputFile ".\installer.ps1" `
-              -outputFile ".\bin\SignalRGB_Installer.exe" `
-              -icon ".\icon.ico" `
-              -title "SRGB Effect Installer" `
+Invoke-ps2exe -inputFile "$PSScriptRoot\installer.ps1" `
+              -outputFile "$PSScriptRoot\bin\SignalRGB_Installer.exe" `
+              -icon "$PSScriptRoot\rgbjunkielogo2.ico" `
+              -title "RGBJunkie Effect Installer" `
               -description "Utility to install custom effects and components to SignalRGB" `
               -company "José Miranda" `
               -version "1.6.0.0" `
